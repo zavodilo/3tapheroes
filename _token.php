fd73fb9d9a5375591202a045d2320ce5af3a0e07
Alessio Calì <Alessio Cal>
2016-10-08 18:14:50 +0200

    Update token.php

--- _token.php
+++ _token.php
@@ -1,6 +1,6 @@
 <?php
     //Put your bot related informations here
-    $APP_NAME = "Your App Name Here";
-    $BOT_TOKEN = "1234:MyAwesomeBot";
-    $WEBHOOK = ['url' => 'https://example.com/<token>/webhook']; //https://telegram-bot-sdk.readme.io/docs/setwebhook
+    $APP_NAME = "Three Tap Heroes";
+    $BOT_TOKEN = "281292155:AAF_xh7KSzKQdZ4BR7G6Xa5kTB2_AGePqos";
+    $WEBHOOK = ['url' => 'https://threetapheroes.herokuapp.com/execute.php']; //https://telegram-bot-sdk.readme.io/docs/setwebhook
 ?>
